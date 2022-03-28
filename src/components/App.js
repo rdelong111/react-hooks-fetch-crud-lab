3f317d797df195436c16ce3aa37819d9a0f91319
Ryan DeLong <rdelong111@gmail.com>
2022-03-27 23:46:06 -0500

    commit tree c16c037caa218bb2ab4d9d7dd5af338848b6ab55

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [theQs, updateQs] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then((r) => r.json())
+      .then((questions) => updateQs(questions));
+  }, []);
+
+  function handleNewQ(newQ) {
+    updateQs([...theQs, newQ]);
+  }
+
+  function handleDeleteQ(theQ) {
+    const newQList = theQs.filter((question) => question.id !== theQ.id);
+    updateQs(newQList);
+  }
+
+  function handleUpdatedQ(theQ) {
+    const updatedQList = theQs.map((question) => {
+      if (question.id === theQ.id) return theQ;
+      else return question;
+    });
+    updateQs(updatedQList);
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onNewQ={handleNewQ} /> : <QuestionList questions={theQs} onDeleteQ={handleDeleteQ} onUpdateQ={handleUpdatedQ} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,48 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionList({questions, onDeleteQ, onUpdateQ}) {
+  const theQs = questions.map((question) => (
+    <li key={`${question.id}${Math.random()}`}>
+      {question.prompt}
+      <label>
+        Correct Answer
+        <select onChange={(e) => handleCorrectChange(e, question.id)} value={question.correctIndex}>
+          {question.answers.map((answer, index) => (
+            <option key={`${question.id}${answer}`} value={index}>{answer}</option>
+          ))}
+        </select>
+      </label>
+      <button onClick={() => handleDelete(question)}>Delete Question</button>
+    </li>
+  ));
+
+  function handleDelete(question) {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: 'DELETE'
+    })
+      .then(() => onDeleteQ(question));
+  }
+
+  function handleCorrectChange(e, ID) {
+    fetch(`http://localhost:4000/questions/${ID}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'correctIndex': e.target.value
+      })
+    })
+      .then((r) => r.json())
+      .then((updatedQ) => {
+        onUpdateQ(updatedQ);
+      });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{theQs}</ul>
     </section>
   );
 }
